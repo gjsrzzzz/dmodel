6e5e49c1a89d5133e1938a6030db610e14760242
Graham Robertson <Graham@robertson.net>
2018-01-28 00:46:10 +0000

    heirarchy insert

---
+++ includeweb/src/main/java/com/jalindi/includeweb/IncludewebApplication.java
@@ -0,0 +1,13 @@
+package com.jalindi.includeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class IncludewebApplication {
+	public IncludewebApplication()
+	{
+		System.out.println("xxx");
+	}
+
+}
